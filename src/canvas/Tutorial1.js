f9600029cce65bd78ac3f5f31278911422be73a9
unknown <sonwj0915@naver.com>
2021-04-12 19:00:02 +0900

    모든 벽면에 튕기기

--- src/canvas/Tutorial1.js
+++ src/canvas/Tutorial1.js
@@ -41,14 +41,16 @@ function Tutorial1() {
     // }
 
     var x = 200;
+    var y = 200;
     var dx = 4;
+    var dy = 4;
     var radius = 30;
     function animate() {
       requestAnimationFrame(animate);
       ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
       ctx.beginPath();
-      ctx.arc(x, 200, 30, 0, Math.PI * 2, false);
+      ctx.arc(x, y, 30, 0, Math.PI * 2, false);
       ctx.strokeStyle = "blue";
       ctx.stroke();
 
@@ -56,7 +58,12 @@ function Tutorial1() {
         dx = -dx;
       }
 
+      if (y + radius > window.innerHeight || y - radius < 0) {
+        dy = -dy;
+      }
+
       x += dx;
+      y += dy;
     }
 
     animate();
