4fde5b76d54b1c76430f149ad1d23b9909d895f2
unknown <sonwj0915@naver.com>
2021-04-13 14:37:30 +0900

    삭제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Tutorial1 from "./canvas/Tutorial1";
-import PixiExam from "./canvas/PixiExam";
 
 function App() {
   return (
     <>
-      <PixiExam />
+      <Tutorial1 />
     </>
   );
 }
--- src/canvas/PixiExam.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PixiExam() {
-  return <div>Hello</div>;
-}
-
-export default PixiExam;
