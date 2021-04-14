8e027e0cb889a369d530334007cceb9d65051d48
unknown <sonwj0915@naver.com>
2021-04-14 21:21:24 +0900

    새로운 거 준비

--- src/canvas/tools/example/circle.js
+++ src/canvas/tools/example/circle.js
@@ -1,16 +1,19 @@
 export function Circle(ctx) {
+  const colorArray = ['#9de0ff', '#57ACE2', '#0072B5', '#254969'];
+
   function makeCircle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.color = colorArray[parseInt(Math.random() * colorArray.length)];
 
     this.draw = function () {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      ctx.strokeStyle = 'blue';
-      ctx.stroke();
+      ctx.fillStyle = this.color;
+      ctx.fill();
     };
 
     this.update = function () {
@@ -38,7 +41,7 @@ export function Circle(ctx) {
   var circleArray = [];
 
   for (var i = 0; i < 100; i++) {
-    var radius = 30;
+    var radius = Math.random() * 250 + 10;
     var x = Math.random() * (window.innerWidth - radius * 2) + radius;
     var y = Math.random() * (window.innerHeight - radius * 2) + radius;
     var dx = (Math.random() - 0.5) * 2;
