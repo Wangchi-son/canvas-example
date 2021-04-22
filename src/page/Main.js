10f4380f8309f607bed74eea3393ee2218221e6b
unknown <sonwj0915@naver.com>
2021-04-22 12:41:50 +0900

    mainpage 생성

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './css/Main.css';
-import Puppy from '../static/images/puppy.png';
-import styled from 'styled-components';
-
-const Img = styled.img`
-  width: 100px;
-`;
+import gsap from 'gsap/gsap-core';
 
 function Main() {
+  useEffect(() => {
+    gsap.to('#profile', {
+      opacity: 0.5,
+      duration: 1
+    });
+    document.getElementById('profile').addEventListener('mouseover', () => {
+      gsap.to('#profile', {
+        opacity: 1
+      });
+    });
+    document.getElementById('profile').addEventListener('mouseout', () => {
+      gsap.to('#profile', {
+        opacity: 0.5
+      });
+    });
+  });
+
   return (
     <div id="mainPage">
-      <Img src={Puppy} alt="me"></Img>
       <h1 id="mainTitle">hello</h1>
+      <div id="profile">
+        <div className="background-image1"></div>
+        <div id="hover"></div>
+      </div>
+      <div id="square"></div>
     </div>
   );
 }
