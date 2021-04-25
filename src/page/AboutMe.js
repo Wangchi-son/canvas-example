00c2fb473a526108a5c97a56906ea82c2342e941
unknown <sonwj0915@naver.com>
2021-04-25 19:17:54 +0900

    horizon 효과 됐다 ㅠ

--- src/page/AboutMe.js
+++ src/page/AboutMe.js
@@ -4,10 +4,13 @@ import React, { useEffect } from 'react';
 import './css/AboutMe.css';
 import HomeButton from './tools/HomeButton';
 
+import Motivation from '../static/images/motivation.jpg';
+
 function AboutMe() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
+    // anchor 옵션
     const panelsSection = document.querySelector('.overX');
     const panelsContainer = document.querySelector('.row');
 
@@ -44,7 +47,35 @@ function AboutMe() {
       });
     });
 
+    // 패널 옵션
+
     const sections = gsap.utils.toArray('.panel');
+
+    // // 프로필 move 옵션
+    // gsap.to('.profile', {
+    //   xPercent: -100 * (sections.length - 1.5),
+    //   ease: 'none',
+    //   scrollTrigger: {
+    //     trigger: '.overX',
+    //     pin: true,
+    //     scrub: 1,
+    //     start: 'top top',
+    //     end: () => '+=' + document.querySelector('.overX').offsetWidth
+    //   },
+    //   left: 0,
+    //   duration: 1
+    // });
+    let maxWidth = 0;
+
+    const getMaxWidth = () => {
+      maxWidth = 0;
+      sections.forEach((section) => {
+        maxWidth += section.offsetWidth;
+      });
+    };
+    getMaxWidth();
+
+    console.log(sections[0].offsetWidth);
 
     gsap.to(sections, {
       xPercent: -100 * (sections.length - 1),
@@ -54,8 +85,25 @@ function AboutMe() {
         pin: true,
         scrub: 1,
         snap: 1 / (sections.length - 1),
-        end: () => '+=' + document.querySelector('.overX').offsetWidth
+        end: () => `+=${maxWidth}`,
+        invalidateOnRefresh: true
       }
+    });
+
+    ScrollTrigger.addEventListener('refreshInit', getMaxWidth);
+
+    sections.forEach((sct, i) => {
+      ScrollTrigger.create({
+        trigger: sct,
+        start: () =>
+          `top top-=${
+            (sct.offsetLeft - window.innerWidth / 2) *
+            (maxWidth / (maxWidth - window.innerWidth))
+          }`,
+        end: () =>
+          `+=${sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth))}`,
+        toggleClass: { targets: sct, className: 'move' + i }
+      });
     });
   });
   return (
@@ -72,20 +120,20 @@ function AboutMe() {
           &sdot;
         </a>
       </div>
-      <div className="overX ">
+      <div className="overX">
         <div className="row">
-          <div className="box a panel" id="panel-1">
-            <h1 className="mainTitle">
-              <p>I’m a Newcomer</p>
-              <p>Frontend Developer,</p>
-            </h1>
-            <div className="profile"></div>
-          </div>
-          <div className="box b panel" id="panel-2">
-            panel 2
-          </div>
+          <div className="box a panel" id="panel-1"></div>
+          <div className="box b panel" id="panel-2"></div>
+          <h1 className="mainTitle">
+            <p>
+              I’m a Newcomer
+              <br />
+              Frontend Developer,
+            </p>
+          </h1>
+          <div className="profile"></div>
           <div className="box c panel " id="panel-3">
-            panel 3
+            panel 3<img src={Motivation} alt="motivation" id="motivation"></img>
           </div>
         </div>
       </div>
