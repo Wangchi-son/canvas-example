a8779d9436b0ac272f91ebe3a7734570896044ac
unknown <sonwj0915@naver.com>
2021-04-27 15:23:19 +0900

    야매 아니고 진짜로 토글액션 해결

--- src/page/AboutMe.js
+++ src/page/AboutMe.js
@@ -77,6 +77,12 @@ function AboutMe() {
     ScrollTrigger.addEventListener('refreshInit', getMaxWidth);
 
     // 클래스 추가
+    const titleList = [
+      "I'm a<br /> Beginner<br/> Frontend<br /> Developer",
+      'Introduce <br/> My self',
+      'Motivation'
+    ];
+    const colorList = ['rgb(255,255,255)', 'rgb(255,255,255)', 'rgb(0,0,0)'];
 
     sections.forEach((sct, i) => {
       ScrollTrigger.create({
@@ -88,85 +94,115 @@ function AboutMe() {
           }`,
         end: () =>
           `+=${sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth))}`,
-        toggleClass: { targets: sct, className: 'move' + i }
+        toggleClass: { targets: sct, className: 'move' + i },
+        onEnter: () => {
+          console.log(`anc${i}`);
+          gsap.to('.p', {
+            duration: 1.4,
+            text: titleList[i]
+          });
+          gsap.to(`.anc${i}`, {
+            opacity: 1
+          });
+          gsap.to(`.stateBlack${i}`, {
+            color: 'rgb(0,0,0)'
+          });
+        },
+        onEnterBack: () => {
+          gsap.to('.p', {
+            duration: 1.4,
+            text: titleList[i]
+          });
+          gsap.to(`.anc${i}`, {
+            opacity: 1
+          });
+          gsap.to(`.stateWhite${i}`, {
+            color: 'rgb(255,255,255)'
+          });
+        },
+        onLeave: () => {
+          gsap.to(`.anc${i}`, {
+            opacity: 0.7
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to(`.anc${i}`, {
+            opacity: 0.7
+          });
+        }
       });
     });
 
-    // 텍스트 효과
-    document.querySelector('#panel-1').addEventListener('mouseover', () => {
-      gsap.to('.p', {
-        duration: 1.4,
-        text: "I'm a<br /> Beginner<br/> Frontend<br /> Developer"
-      });
-    });
-    document.querySelector('#panel-2').addEventListener('mouseover', () => {
-      gsap.to('.p', {
-        duration: 1,
-        text: 'Introduce <br/> My self'
-      });
+    const anchors = gsap.utils.toArray('.anchor');
 
-      gsap.to('.anchor', {
-        color: 'rgb(255,255,255)'
-      });
-    });
-    document.querySelector('#panel-3').addEventListener('mouseover', () => {
-      gsap.to('.p', {
-        duration: 1,
-        text: 'Motivation'
-      });
-      gsap.to('.anchor', {
-        color: 'rgb(0,0,0)'
-      });
-    });
-
-    // anchor 텍스트 효과
-    document.querySelector('.anc1').addEventListener('click', () => {
-      gsap.to('.p', {
-        duration: 1.4,
-        text: "I'm a<br /> Beginner<br/> Frontend<br /> Developer"
-      });
-      gsap.to('.anchor', {
-        color: 'rgb(255,255,255)'
-      });
-    });
-    document.querySelector('.anc2').addEventListener('click', () => {
-      gsap.to('.p', {
-        duration: 1,
-        text: 'Introduce <br/> My self'
-      });
-      gsap.to('.anchor', {
-        color: 'rgb(255,255,255)'
-      });
-    });
-    document.querySelector('.anc3').addEventListener('click', () => {
-      gsap.to('.p', {
-        duration: 1,
-        text: 'Motivation'
-      });
-      gsap.to('.anchor', {
-        color: 'rgb(0,0,0)'
-      });
-    });
+    // anchors.forEach((anc, i) => {
+    //   ScrollTrigger.create({
+    //     trigger: sections[i],
+    //     start: () =>
+    //       `top top-=${
+    //         (sections[i].offsetLeft - window.innerWidth / 2) *
+    //         (maxWidth / (maxWidth - window.innerWidth))
+    //       }`,
+    //     end: () =>
+    //       `+=${
+    //         sections[i].offsetWidth *
+    //         (maxWidth / (maxWidth - window.innerWidth))
+    //       }`,
+    //     toggleClass: { targets: anc, className: 'now' }
+    //   });
+    // });
   });
 
   return (
     <div className="mainContainer">
       <HomeButton />
       <div id="navbar">
-        <a href="#panel-1" className="anchor anc1">
+        <a href="#panel-1" className="anchor anc0 stateWhite1 stateBlack2">
           &sdot;
         </a>
-        <a href="#panel-2" className="anchor anc2">
+        <a href="#panel-2" className="anchor anc1 stateWhite1 stateBlack2">
           &sdot;
         </a>
-        <a href="#panel-3" className="anchor anc3">
+        <a href="#panel-3" className="anchor anc2 stateWhite1 stateBlack2">
           &sdot;
         </a>
       </div>
       <div className="overX">
         <div className="row">
           <div className="box a panel" id="panel-1"></div>
-          <div className="box b panel" id="panel-2"></div>
+          <div className="box b panel" id="panel-2">
+            <div id="introBox">
+              <h3 className="introduce" id="name">
+                이름:<span> 손원재</span>
+              </h3>
+              <h3 className="introduce" id="age">
+                나이:<span> 26세</span>
+              </h3>
+              <h3 className="introduce" id="academicBg">
+                학력:<span> 계명대학교 졸업</span>
+              </h3>
+              <h3 className="introduce" id="major">
+                전공:<span> 산업디자인 학과</span>
+              </h3>
+              <h3 className="introduce" id="major">
+                수상 경력:<span> 희망이음 경진대회 (동상) 수상</span>
+              </h3>
+              <h3 className="introduce" id="major">
+                코딩 경험:
+                <span>
+                  &nbsp;교내 IT융합교육센터 주관 코딩집중캠프 참여
+                  <br />
+                  &ensp;&ensp;&ensp;&ensp;&ensp;&emsp;&emsp;패스트캠퍼스
+                  온라인강의 수강
+                </span>
+              </h3>
+              <h3 className="introduce" id="major">
+                자격증:
+                <span> MOS MASTER</span>
+              </h3>
+            </div>
+          </div>
+
           <h1 className="mainTitle">
             <p className="p">
               I'm a<br /> Beginner
@@ -175,8 +211,9 @@ function AboutMe() {
             </p>
           </h1>
           <div className="profile"></div>
+
           <div className="box c panel " id="panel-3">
-            panel 3<img src={Motivation} alt="motivation" id="motivation"></img>
+            <img src={Motivation} alt="motivation" id="motivation"></img>
           </div>
         </div>
       </div>
